28fc47d0a1ea49d293e7b79e9ebd3a32e0ffffb4
shaun-frost <31550536+shaun-frost@users.noreply.github.com>
2017-10-02 16:52:32 +0530

    fixed indentation

--- src/script.js
+++ src/script.js
@@ -2,75 +2,65 @@ let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const timerButtons = document.querySelectorAll('.timer__button');
-var audio = new Audio("AMemoryAway.ogg") ;
+const audio = new Audio('AMemoryAway.ogg');
 
-//audio.oncanplaythrough = function(){
-//audio.play();
-//}
-//
-//
-//audio.onended = function(){
-//audio.play();
-//}
-// Function that handles our counter
-    function notifyMe() {
-  // Let's check if the browser supports notifications
-    if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification("Time's Up!");
-  }
+// Function for desktop notifications
+function notifyMe() {
+	// Let's check if the browser supports notifications
+	if (!('Notification' in window)) {
+		alert('This browser does not support desktop notification');
+	}
+	// Let's check whether notification permissions have already been granted
+	else if (Notification.permission === 'granted') {
+		// If it's okay let's create a notification
+		const notification = new Notification('Time\'s Up!');
+	}
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Time's Up!");
-      }
-    });
-  }
+	// Otherwise, we need to ask the user for permission
+	else if (Notification.permission !== 'denied') {
+		Notification.requestPermission(permission => {
+		// If the user accepts, let's create a notification
+			if (permission === 'granted') {
+				const notification = new Notification('Time\'s Up!');
+			}
+		});
+	}
 }
 
+// Function that handles our counter
 function timer(seconds) {
-
 	// Reset any ongoing timers
 	clearInterval(countdown);
-    audio.pause();
-    audio.currentTime = 0;
+	audio.pause();
+	audio.currentTime = 0;
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	displayTimeLeft(seconds);
 	displayEndTime(then);
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
-		// check if we should stop it
-		if(secondsLeft < 0) {
+		// Check if we should stop it
+		if (secondsLeft < 0) {
 			clearInterval(countdown);
 			return;
 		}
 		displayTimeLeft(secondsLeft);
-	}, 1000)
+	}, 1000);
 }
-
 
 // Function to display the remaining time
 function displayTimeLeft(seconds) {
 	const hour = Math.floor(seconds / 3600);
-	const minutes = Math.floor( (seconds % 3600) / 60);
+	const minutes = Math.floor((seconds % 3600) / 60);
 	const remainderSeconds = seconds % 60;
 	const display = `${hour < 10 ? '0' : ''}${hour}:${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
 	timerDisplay.textContent = display;
 	document.title = display;
-	if (hour == 0 && minutes == 0 && remainderSeconds == 0)
-		{
-            document.title = "Time's Up !";
-            notifyMe();
-            audio.play();
-        }
+	if (hour === 0 && minutes === 0 && remainderSeconds === 0) {
+		document.title = 'Time\'s Up !';
+		notifyMe();
+		audio.play();
+	}
 }
 
 // Function to display time of return
@@ -81,15 +71,14 @@ function displayEndTime(timestamp) {
 	endTime.textContent = `Be Back At - ${hours > 12 ? hours - 12 : hours}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
-
 // Listening for various events
 timerButtons.forEach(timerButton => timerButton.addEventListener('click', () => {
-	timer(timerButton.dataset['time']);
+	timer(timerButton.dataset.time);
 }));
 
-document.customForm.addEventListener('submit', function(e) {
+document.customForm.addEventListener('submit', function (e) {
 	e.preventDefault();
-	if(this.minutes.value > 0){
+	if (this.minutes.value > 0) {
 		timer(this.minutes.value * 60);
 	} else {
 		alert('Invalid Input');
